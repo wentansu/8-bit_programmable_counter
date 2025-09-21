a39b0ffea9824cbf78680d8f9328ad8e393f88a0
wentansu <suwentan000@hotmail.com>
2025-09-21 12:50:47 -0400

    Update counter.v

--- src/counter.v
+++ src/counter.v
@@ -1,28 +1,36 @@
 module tt_um_wentansu_counter (
-    input wire clk,
-    input wire rst_n,
-    input wire [7:0] load,
-    input wire load_enable,
-    input wire increment,
-    input wire out_enable,
-    output reg [7:0] value,
-    output wire [7:0] out
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
+
+    wire load = ui_in[0];
+    wire increment = ui_in[1];
+    wire out_enable = ui_in[2];
+
+    reg [7:0] value = 8'b0;
 
     always @(posedge clk) begin
         if (!rst_n) begin
-            value <= 8'b0;
+            value[7:0] <= 8'b0;
         end
 
-        else if (load_enable) begin
-            value <= load;
+        else if (load) begin
+            value[7:0] <= uio_in[7:0];
         end
 
         else if (increment) begin
-            value <= value + 8'b1;
+            value[7:0] <= value[7:0] + 8'b1;
         end
     end
 
-    assign out = value;
+    assign uo_out[7:0] = out_enable ? value[7:0] : 8'b0;
+    assign uio_out = 0;
+    assign uio_oe = 0;
 
 endmodule
